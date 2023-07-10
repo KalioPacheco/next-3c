1a8f4f76afa04c831904cdbd1e30ec249da3018a
José Miguel Carrera <jm.carrera@lapieza.io>
2023-07-10 08:57:35 -0600

    files updated

--- src/services/firebase/auth.js
+++ src/services/firebase/auth.js
@@ -1,20 +1,38 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-const auth = getAuth();
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import app from "./"
 
 function login(email, password) {
+  const auth = getAuth(app);
+
   signInWithEmailAndPassword(auth, email, password)
 
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
+    console.log(user)
     // ...
   })
 
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
+    console.log(errorCode, errorMessage)
   });
 }
 
-export { login }
+function register(email, password) {
+  const auth = getAuth(app);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+}
+
+export { login, register }
